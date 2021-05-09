55658a45b6378eb4d4253af4f18a2ceaa6cdf78a
WangXiao <39123418+MoonStartMan@users.noreply.github.com>
2021-05-09 22:43:53 +0800

    2021-05-09给一个女孩的写的DEMO

---
+++ 2021-05-09DEMO/js/common.js
@@ -0,0 +1,13 @@
+(function(doc,win){
+    var doc = doc.documentElement;
+    window.onload = function() {
+        Resize();
+    }
+    // 当DOM加载后执行
+    win.addEventListener('resize', Resize, false);
+    // 当屏幕发生变化时执行
+    function Resize(){
+        doc.style.fontSize=doc.clientWidth/23.4375 +'px'; //屏幕宽度为1360时 html font-size=16px;
+        //120为1920/16计算得出的值，当编写浏览器发生变化时可用编写的浏览器宽度/16得出这个固定值;
+    }
+})(document,window)
